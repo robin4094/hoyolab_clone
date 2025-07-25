8658108062cb8d893c04f4a6c1df1a60317eb14a
robin4094 <hwangrb0310@gmail.com>
2025-07-24 20:04:13 -0700

    day 3

---
+++ main.js
@@ -0,0 +1,34 @@
+console.log("hello world!");
+console.log(1 + 7)
+console.log(7 * 9)
+const pi = 3.141592654
+const someBoolean = true;
+console.log(someBoolean)
+const myName = "Robin";
+console.log(myName);
+
+if (someBoolean) {
+    console.log("Someboolean is true")
+    console.log("local scope")
+}
+print("global scope")
+let idx = 0;
+while (idx < 100) {
+    console.log(idx);
+    idx++; // idx += 1
+}
+
+// function some_function() {
+//     console.log("somefunction is runnin");
+// }
+// const someArray = [1, 2, 3, 4, 5, 6, 7, 8];
+// someArray.forEach((element) => console.log(element));
+// for (let i = 0; i < someArray.length; i++) {
+//     console.log(someArray[i]);
+// }
+
+
+
+
+
+
